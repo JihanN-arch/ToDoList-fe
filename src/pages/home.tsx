e94d327e1cb50a2607164abe35dd2bf51303b19d
JN <jnpermata07@gmail.com>
2026-01-15 11:55:27 +0700

    idk

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-center text-3xl font-bold">Hallo User :D</h1>
+      <p className="text-center">To do list web</p>
     </div>
   );
 }
