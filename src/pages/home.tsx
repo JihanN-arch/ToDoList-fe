0816aef09bb840dcdbf29753eb7e4cb56c9ff13b
Naurah <184463973+Naur4h@users.noreply.github.com>
2026-01-15 12:59:55 +0700

    tulisan sesuai figma

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,17 @@
 export default function Home() {
   return (
-    <div className = "m-10">
-
+    <div >
+   
     <div>
-      <h1 className="text-left text-3xl font-bold">Hello User</h1>
-      <p className="text-left">Explore your To-Do-List</p>
+      <h1 className="absolute top-20 left-[73px] w-[248px] h-[62px] text-left font-inter font-bold text-[48px] leading-[100%] tracking-[0%] text-black">Hello User</h1>
+      <p className="absolute top-[137px] left-[74px] w-[250px] h-[27px] text-left font-inter font-normal text-[22px] leading-[100%] tracking-[0%]">Explore your To-Do-List</p>
+      <p className="absolute top-[190px] left-[73px] w-[225px] h-[21px] text-left font-inter font-bold text-[14px] leading-[100%] tracking-[0%]">Today Task</p>
+    </div>
+
+  
+    <div>
+      
+
     </div>
     
     </div>
